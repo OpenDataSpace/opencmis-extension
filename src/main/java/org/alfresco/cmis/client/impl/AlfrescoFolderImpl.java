b099eb272f682992ec3d006371f6726c8e42c10a
mark.rogers@alfresco.com <mark.rogers@alfresco.com@896628b3-2344-bb7d-e986-543e3a70545d>
2014-08-14 17:03:24 +0000

    Version 1.0 - works with open cmis 0.11.0
    git-svn-id: https://svn.codespot.com/a/apache-extras.org/alfresco-opencmis-extension/trunk@43 896628b3-2344-bb7d-e986-543e3a70545d

--- src/main/java/org/alfresco/cmis/client/impl/AlfrescoFolderImpl.java
+++ src/main/java/org/alfresco/cmis/client/impl/AlfrescoFolderImpl.java
@@ -23,7 +23,6 @@ import org.apache.chemistry.opencmis.client.api.CmisObject;
 import org.apache.chemistry.opencmis.client.api.ObjectId;
 import org.apache.chemistry.opencmis.client.api.ObjectType;
 import org.apache.chemistry.opencmis.client.api.OperationContext;
-import org.apache.chemistry.opencmis.client.api.TransientCmisObject;
 import org.apache.chemistry.opencmis.client.runtime.FolderImpl;
 import org.apache.chemistry.opencmis.client.runtime.SessionImpl;
 import org.apache.chemistry.opencmis.commons.data.ObjectData;
@@ -48,14 +47,6 @@ public class AlfrescoFolderImpl extends FolderImpl implements AlfrescoFolder
         aspects = new AlfrescoAspectsImpl(session, this);
     }
 
-    @Override
-    protected TransientCmisObject createTransientCmisObject()
-    {
-        TransientAlfrescoFolderImpl td = new TransientAlfrescoFolderImpl();
-        td.initialize(getSession(), this);
-
-        return td;
-    }
 
     public ObjectType getTypeWithAspects()
     {
